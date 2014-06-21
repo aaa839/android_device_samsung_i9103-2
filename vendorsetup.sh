4a143366f234076343932b67d8dfcad4432e664b
Nikhil <nikhil@menghani.com>
2014-06-21 11:09:01 +0530

    removed unnecessary patch in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -36,14 +36,15 @@ else
 	echo "     [FAIL]"
 fi
 
-echo -n "Apply patch 0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch"
-(cd frameworks/native; git am ../../device/samsung/i9103/patches/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-	echo "     [DONE]"
-else
-	(cd frameworks/native; git am --abort)
-	echo "     [FAIL]"
-fi
+#echo -n "Apply patch 0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch"
+#(cd frameworks/native; git am ../../device/samsung/i9103/patches/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch) > /dev/null 2>&1
+#if [ $? == 0 ]; then
+#	echo "     [DONE]"
+#else
+#	(cd frameworks/native; git am --abort)
+#	echo "     [FAIL]"
+#fi
+
 echo -n "Apply patch 0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
 (cd frameworks/av; git am ../../device/samsung/i9103/patches/0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
 if [ $? == 0 ]; then
@@ -54,14 +55,14 @@ else
 fi
 
 
-echo -n "Apply patch 0001-external-skia-patch.patch"
-(cd external/skia; git am ../../device/samsung/i9103/patches/0001-external-skia-patch.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-	echo "     [DONE]"
-else
-	(cd external/skia; git am --abort)
-	echo "     [FAIL]"
-fi
+#echo -n "Apply patch 0001-external-skia-patch.patch"
+#(cd external/skia; git am ../../device/samsung/i9103/patches/0001-external-skia-patch.patch) > /dev/null 2>&1
+#if [ $? == 0 ]; then
+#	echo "     [DONE]"
+#else
+#	(cd external/skia; git am --abort)
+#	echo "     [FAIL]"
+#fi
 
 echo "Apply patch to frameworks/native"
 echo -n "Apply patch 0001-Fix-layer-dump-for-tegra2.patch"
