330855d456086885ba4cbd382d3f687f41880383
Nikhil <nikhil@menghani.com>
2014-07-29 20:49:33 +0530

    added carbon specific changes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_i9103.mk
+    $(LOCAL_DIR)/full_i9103.mk \
+    $(LOCAL_DIR)/carbon.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,72 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo rom_i9103-userdebug
+add_lunch_combo carbon_i9103-userdebug
+
+echo "Apply patch to frameworks/base"
+echo -n "Apply patch 0001-framework-base-patch.patch"
+(cd frameworks/base; git am ../../device/samsung/i9103/patches/carbon/0001-framework-base-patch.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/base; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo -n "Apply patch 0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch"
+(cd frameworks/native; git am ../../device/samsung/i9103/patches/carbon/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/native; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to frameworks/native"
+echo -n "Apply patch 0003-Fix-layer-dump-for-tegra2.patch"
+(cd frameworks/native; git am ../../device/samsung/i9103/patches/carbon/0003-Fix-layer-dump-for-tegra2.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+       echo "     [DONE]"
+else
+       (cd frameworks/native; git am --abort)
+       echo "     [FAIL]"
+fi
+
+echo -n "Apply patch 0004-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
+(cd frameworks/av; git am ../../device/samsung/i9103/patches/carbon/0004-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/av; git am --abort)
+	echo "     [FAIL]"
+fi
+
+
+echo -n "Apply patch 0005-external-skia-patch.patch"
+(cd external/skia; git am ../../device/samsung/i9103/patches/carbon/0005-external-skia-patch.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd external/skia; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to external/chromium_org"
+echo -n "Apply patch 0006-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch"
+(cd external/chromium_org; git am ../../device/samsung/i9103/patches/carbon/0006-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+  echo "     [DONE]"
+else
+  (cd external/chromium_org; git am --abort)
+  echo "     [FAIL]"
+fi
+
+echo "Apply patch to bionic"
+echo -n "Apply patch 0007-Add-tegra2-to-bionic.patch"
+(cd bionic; git am ../device/samsung/i9103/patches/carbon/0007-Add-tegra2-to-bionic.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+    echo "     [DONE]"
+else
+    (cd bionic; git am --abort)
+    echo "     [FAIL]"
+fi
