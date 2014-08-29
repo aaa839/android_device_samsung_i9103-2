f93dcf421bc1cae62f2141297f1509b92c2572c6
Nikhil <nikhil@menghani.com>
2014-08-29 15:53:43 +0530

    pa patches

--- Rom Specific Files/AOSPA/vendorsetup.sh
+++ Rom Specific Files/AOSPA/vendorsetup.sh
@@ -24,7 +24,7 @@ add_lunch_combo pa_i9103-userdebug
 
 echo "Apply patch to frameworks/base"
 echo -n "Apply patch 0001-framework-base-patch.patch"
-(cd frameworks/base; git am ../../device/samsung/i9103/patches/carbon/0001-framework-base-patch.patch) > /dev/null 2>&1
+(cd frameworks/base; git am ../../device/samsung/i9103/patches/aospa/0001-framework-base-patch.patch) > /dev/null 2>&1
 if [ $? == 0 ]; then
 	echo "     [DONE]"
 else
@@ -33,7 +33,7 @@ else
 fi
 
 echo -n "Apply patch 0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch"
-(cd frameworks/native; git am ../../device/samsung/i9103/patches/carbon/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch) > /dev/null 2>&1
+(cd frameworks/native; git am ../../device/samsung/i9103/patches/aospa/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch) > /dev/null 2>&1
 if [ $? == 0 ]; then
 	echo "     [DONE]"
 else
@@ -43,7 +43,7 @@ fi
 
 echo "Apply patch to frameworks/native"
 echo -n "Apply patch 0003-Fix-layer-dump-for-tegra2.patch"
-(cd frameworks/native; git am ../../device/samsung/i9103/patches/carbon/0003-Fix-layer-dump-for-tegra2.patch) > /dev/null 2>&1
+(cd frameworks/native; git am ../../device/samsung/i9103/patches/aospa/0003-Fix-layer-dump-for-tegra2.patch) > /dev/null 2>&1
 if [ $? == 0 ]; then
        echo "     [DONE]"
 else
@@ -52,7 +52,7 @@ else
 fi
 
 echo -n "Apply patch 0004-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
-(cd frameworks/av; git am ../../device/samsung/i9103/patches/carbon/0004-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
+(cd frameworks/av; git am ../../device/samsung/i9103/patches/aospa/0004-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
 if [ $? == 0 ]; then
 	echo "     [DONE]"
 else
@@ -62,7 +62,7 @@ fi
 
 
 echo -n "Apply patch 0005-external-skia-patch.patch"
-(cd external/skia; git am ../../device/samsung/i9103/patches/carbon/0005-external-skia-patch.patch) > /dev/null 2>&1
+(cd external/skia; git am ../../device/samsung/i9103/patches/aospa/0005-external-skia-patch.patch) > /dev/null 2>&1
 if [ $? == 0 ]; then
 	echo "     [DONE]"
 else
@@ -72,7 +72,7 @@ fi
 
 echo "Apply patch to external/chromium_org"
 echo -n "Apply patch 0006-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch"
-(cd external/chromium_org; git am ../../device/samsung/i9103/patches/carbon/0006-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch) > /dev/null 2>&1
+(cd external/chromium_org; git am ../../device/samsung/i9103/patches/aospa/0006-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch) > /dev/null 2>&1
 if [ $? == 0 ]; then
   echo "     [DONE]"
 else
@@ -80,12 +80,12 @@ else
   echo "     [FAIL]"
 fi
 
-echo "Apply patch to bionic"
-echo -n "Apply patch 0007-Add-tegra2-to-bionic.patch"
-(cd bionic; git am ../device/samsung/i9103/patches/carbon/0007-Add-tegra2-to-bionic.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-    echo "     [DONE]"
-else
-    (cd bionic; git am --abort)
-    echo "     [FAIL]"
-fi
+#echo "Apply patch to bionic"
+#echo -n "Apply patch 0007-Add-tegra2-to-bionic.patch"
+#(cd bionic; git am ../device/samsung/i9103/patches/aospa/0007-Add-tegra2-to-bionic.patch) > /dev/null 2>&1
+#if [ $? == 0 ]; then
+#    echo "     [DONE]"
+#else
+#    (cd bionic; git am --abort)
+#    echo "     [FAIL]"
+#fi
